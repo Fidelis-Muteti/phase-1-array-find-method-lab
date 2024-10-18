866fa8f902206640ce8c095a1ea6e60fcff0375b
Fidelis-Muteti <fidelmwovi@gmail.com>
2024-10-18 20:16:39 +0300

    array find lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    // Use the find() method to look for an object with a result of "W"
+    const winRecord = record.find(game => game.result === "W");
+  
+    // If a winRecord is found, return the year; otherwise, return undefined
+    return winRecord ? winRecord.year : undefined;}
